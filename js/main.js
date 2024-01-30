791f000c028897ff00f9759fe2a02bd2b31245fa
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-30 21:38:31 +0100

    add click prev next

--- js/main.js
+++ js/main.js
@@ -3,7 +3,25 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      images: ['./img/01.jpg', './img/02.jpg', './img/03.jpg', './img/04.jpg', './img/05.jpg']
+      images: ['./img/01.jpg', './img/02.jpg', './img/03.jpg', './img/04.jpg', './img/05.jpg'],
+      imageIndex: 0
+    }
+  },
+
+  methods:{
+    increase(){
+      if(this.imageIndex >= this.images.length - 1){
+        this.imageIndex = 0
+      }else{
+        this.imageIndex++
+      }
+    },
+    decrease(){
+      if(this.imageIndex <= 0){
+        this.imageIndex = this.images.length - 1
+      }else{
+        this.imageIndex--
+      }
     }
   }
 }).mount('#app')
